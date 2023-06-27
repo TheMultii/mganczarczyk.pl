13312d0bc829a965f80248040e7f49b6aae7f1aa
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-06-27 20:48:54 +0200

    Portfolio

--- src/components/AnimatedDivider/AnimatedDivider.tsx
+++ src/components/AnimatedDivider/AnimatedDivider.tsx
@@ -2,7 +2,7 @@ import "./AnimatedDivider.css";
 
 const AnimatedDivider = () => {
     return (
-        <div class="z-[500] absolute bottom-16 left-[50%] -translate-x-1/2">
+        <div class="z-[1] absolute bottom-16 left-[50%] -translate-x-1/2">
             <div class="absolute w-px h-full left-0 right-0 ml-auto mr-auto opacity-100 visible transition-all duration-[.7s]">
                 <div class="block w-px h-16 before:content-[''] before:block before:w-px before:top-0 before:bg-white before:h-[50%] scroll-animation"></div>
             </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import AnimatedDivider from "../AnimatedDivider/AnimatedDivider.tsx";
+import AnimatedDivider from "../AnimatedDivider/AnimatedDivider";
 import { images } from "../../assets/images";
 import { createEffect } from "solid-js";
 
@@ -23,14 +23,14 @@ const Header = () => {
     }, []);
 
     return (
-        <div class="relative py-[12rem]">
+        <div class="relative grid h-screen lg:h-[95vh]">
             <img
                 id="bg-image"
                 src="#"
                 alt="bg-image"
-                class="absolute top-0 left-0 z-[-1] w-full h-full object-center invisible"
+                class="absolute top-0 left-0 z-[-1] w-full h-full object-center object-cover invisible"
             />
-            <div class="z-1 flex items-center flex-col gap-y-7">
+            <div class="z-1 flex items-center flex-col gap-y-7 place-content-center">
                 <h1 class="text-6xl text-center">👋</h1>
                 <h2 class="text-4xl text-center font-extrabold">
                     Marcel Gańczarczyk
@@ -42,7 +42,11 @@ const Header = () => {
                     who is not scared of back-end tasks.
                 </h3>
                 <div class="flex flex-row mt-[-.8rem] space-x-3">
-                    <a href="https://github.com/TheMultii" target="_blank">
+                    <a
+                        href="https://github.com/TheMultii"
+                        aria-label="GitHub"
+                        target="_blank"
+                    >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             class="icon icon-tabler icon-tabler-brand-github"
@@ -64,6 +68,7 @@ const Header = () => {
                         </svg>
                     </a>
                     <a
+                        aria-label="GitLab"
                         href="https://gitlab.mganczarczyk.pl/TheMultii"
                         target="_blank"
                     >
