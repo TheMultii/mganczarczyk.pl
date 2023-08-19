2831a28b0985aad6fd55b77c5cf09f2d090795b6
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-08-19 13:15:09 +0200

    Fixed issue with type mismatch while fetching tairiku images

--- src/components/BlurhashImage/blurhashImageFetch.ts
+++ src/components/BlurhashImage/blurhashImageFetch.ts
@@ -1,6 +1,24 @@
 import { decode } from "blurhash";
 import { Accessor, Setter } from "solid-js";
 
+type TairikuImageResponse = {
+    count: number;
+    images: TairikuImage[];
+};
+
+type TairikuImage = {
+    id: string;
+    hash: string;
+    category: string;
+    nsfw: boolean;
+    dimensions: {
+        width: number;
+        height: number;
+    };
+    source: string;
+    created_at: string;
+};
+
 export const blurhashImageFetch = async (
     img: Accessor<string>,
     setImg: Setter<string>,
@@ -18,19 +36,19 @@ export const blurhashImageFetch = async (
             }
         );
 
-        const data = await response.json();
-        const iamge_url = `https://api.mganczarczyk.pl/tairiku/display/${data.id}`;
+        const data: TairikuImageResponse = await response.json();
+        const image_url = `https://api.mganczarczyk.pl/tairiku/display/${data["images"][0].id}`;
 
         const _img = new Image();
-        _img.src = iamge_url;
+        _img.src = image_url;
         _img.onload = () => {
-            setImg(iamge_url);
+            setImg(image_url);
             requestAnimationFrame(() => {
                 ref.classList.remove("opacity-0", "hidden");
             });
         };
 
-        const pixels = decode(data.hash, 512, 512);
+        const pixels = decode(data["images"][0].hash, 512, 512);
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
         const imageData = ctx.createImageData(512, 512);
